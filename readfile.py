0627b0706575cda7db835b64591708c19da28ff4
weitang114.cml20 <weitang114@gmail.com>
2015-05-14 17:23:24 +0800

    cut lines into words

--- readfile.py
+++ readfile.py
@@ -8,20 +8,20 @@ def path_leaf(path):
     return tail or ntpath.basename(head)
 
 def get_file_name(path):
-    filename = []
+    filelist = []
     for root, dirs, files in os.walk(path):
         for f in files:
-            filename.append(path_leaf(f))
-    return filename
+            filelist.append(os.path.join(root, f))
+            with open(os.path.join(root, f)) as openfile:
+                for line in openfile:
+                    for word in line.split():
+                        print word
+                
+    return filelist
 
 def main():
     txt_path = "/tmp2/weitang114/Holmes_Training_Data/training/"
     file_list = get_file_name(txt_path)
-    i = 0
-    for f in file_list:
-        i = i + 1
-        print f
-        print i
 
 if __name__ == "__main__":
     main()
